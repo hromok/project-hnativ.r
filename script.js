08712176eebccb7934e3af3b5ae3d6bd2178384e
Roman Hnativ <hnativ.dev@gmail.com>
2022-07-27 16:40:31 +0300

    detailed page

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ $(".thumbnail").on("click", function () {
   clicked.addClass("selected");
   $(".primary").empty().append($img.hide().fadeIn("slow"));
 });
+
+$(".thumbnail").on("click", function () {
+  var clicked = $(this);
+  var newSelection = clicked.data("big");
+  var $img = $(".primary").css("background-image", "url(" + newSelection + ")");
+  clicked.parent().find(".thumbnail").removeClass("selected");
+  clicked.addClass("selected");
+  $(".primary").empty().append($img.hide().fadeIn("slow"));
+});
+
+// Counter
+
+let decreaseBtn = document.getElementById("btn-decrease");
+let increaseBtn = document.getElementById("btn-increase");
+let counter = document.getElementById("counter");
+let count = 0;
+
+decreaseBtn.addEventListener('click', ()=>{
+    count --;
+    counter.innerHTML = count;
+});
+
+increaseBtn.addEventListener('click', ()=>{
+    count ++;
+    counter.innerHTML = count;
+})
+
+// Read More Button
+
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.detailed-text')
+
+readMoreBtn.addEventListener('click', (e)=>{
+  text.classList.toggle('show-more');
+  if (readMoreBtn.innerHTML === 'Read More'){
+    readMoreBtn.innerHTML = 'Read Less';
+  } else {
+    readMoreBtn.innerHTML = 'Read More';
+  }
+
+})
